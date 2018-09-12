adf943880f5ec2178963b643903eddba65040d39
Gassmann Flynn <bgassf@bbc-be.local>
2018-09-12 10:32:13 +0200

    Kontakt und Impressum

---
+++ sites/impressum.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Impressum</title>
+
+  </head>
+  <body>
+
+
+
+      <div class="Impressum">
+
+            <h1> Kontakt </h1>
+                <p>
+                <h2>  Website geleitet durch:</h2><br>
+                  Leart Gashi <br>
+                  Flynn Gassmann <br>
+                <h2>  Adresse:</h2><br>
+                  ICT Berufsbildungscenter AG  <br>
+                  Bahnhöheweg 70 <br>
+                  3018 Bern <br>
+                  Schweiz <br>
+                <h2>  Telefon: </h2><br>
+                  079 799 79 79 <br>
+                  077 777 77 77 <br>
+                <h2>  email:  </h2><br>
+                  php@Project.ch <br>
+    </p>
+
+      <h1> Haftungsausschluss </h1>
+
+        <p>
+          Der Autor übernimmt keinerlei Gewähr hinsichtlich der inhaltlichen Richtigkeit, Genauigkeit, Aktualität, Zuverlässigkeit und Vollständigkeit der Informationen.<br>
+          Haftungsansprüche gegen den Autor wegen Schäden materieller oder immaterieller Art, welche aus dem Zugriff oder der Nutzung bzw.<br>
+          Nichtnutzung der veröffentlichten Informationen, durch Missbrauch der Verbindung oder durch technische Störungen entstanden sind, werden ausgeschlossen.<br>
+          Alle Angebote sind unverbindlich.<br>
+          Der Autor behält es sich ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die Veröffentlichung zeitweise oder endgültig einzustellen.<br>
+        </p>
+
+
+      <h1> Urheberrechte </h1>
+
+      <p>
+          Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos oder anderen Dateien auf der Website gehören ausschliesslich Leart Gashi, Flynn Gassmann oder den speziell genannten Rechtsinhabern.<br>
+          Für die Reproduktion jeglicher Elemente ist die schriftliche Zustimmung der Urheberrechtsträger im Voraus einzuholen.<br>
+      </P>
+
+
+      <h1> Datenschutz </h1>
+
+      <p>
+        Gestützt auf Artikel 13 der schweizerischen Bundesverfassung und die datenschutzrechtlichen Bestimmungen des Bundes (Datenschutzgesetz, DSG) hat jede Person Anspruch auf Schutz ihrer Privatsphäre sowie auf Schutz vor Missbrauch ihrer persönlichen Daten.<br>
+        Wir halten diese Bestimmungen ein. Persönliche Daten werden streng vertraulich behandelt und weder an Dritte verkauft noch weiter gegeben.<br>
+        In enger Zusammenarbeit mit unseren Hosting-Providern bemühen wir uns, die Datenbanken so gut wie möglich vor fremden Zugriffen, Verlusten, Missbrauch oder vor Fälschung zu schützen.<br>
+        Beim Zugriff auf unsere Webseiten werden folgende Daten in Logfiles gespeichert: IP-Adresse, Datum, Uhrzeit, Browser-Anfrage und allg. übertragene Informationen zum Betriebssystem resp. Browser.<br>
+        Diese Nutzungsdaten bilden die Basis für statistische, anonyme Auswertungen, so dass Trends erkennbar sind, anhand derer wir unsere Angebote entsprechend verbessern können.<br>
+      </p>
+
+
+
+  </div>
+
+  </body>
+</html>
--- sites/kontakt.php
+++ sites/kontakt.php
@@ -14,6 +14,29 @@
 
 
 
+    <div class="container">
+  <form action="action_page.php">
+
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+    <label for="country">Country</label>
+    <input type="text" id="country" country="country" placeholder="What contry are you from..">
+
+
+    <label for="subject">Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+
+  </form>
+</div>
+
+
+
 
 </body>
 </html>
